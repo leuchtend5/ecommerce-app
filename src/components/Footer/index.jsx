7f755363284b9f6fc713ca56adc7f3af1c6a12f1
leuchtend5 <zxcmj1@gmail.com>
2024-04-28 19:58:59 +0800

    style(home): styling footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,19 +1,57 @@
-import FooterContainer from './styles';
+import { FooterContainer, CopyrightContainer, StyledLogo } from './styles';
+import logo from '../../assets/images/logo/logo.png';
+import { Link } from 'react-router-dom';
+import { IoLogoGithub } from 'react-icons/io';
+import { MdOutlineEmail } from 'react-icons/md';
 
 export default function Footer() {
   return (
     <div>
       <FooterContainer>
         <div className="email-section">
-          <p>Join us</p>
-          <p>Sign in up for 10% discount</p>
-          <input type="text"></input>
-          <button>Sign up</button>
+          <div className="text-content">
+            <p>Catch up with us!</p>
+            <p>Receive $30* off your first purchase when you sign up for our newsletter</p>
+          </div>
+          <div className="input-content">
+            <input type="text"></input>
+            <button>Sign up</button>
+          </div>
         </div>
-        <div className="site-section">Site Map</div>
-        <div className="contact-section">Contact</div>
+        <div className="site-section">
+          <p>Shop</p>
+          <ul>
+            <Link to="/" className="site-link">
+              Electronics
+            </Link>
+            <Link to="/" className="site-link">
+              Jewelery
+            </Link>
+            <Link to="/" className="site-link">
+              Men's Clothing
+            </Link>
+            <Link to="/" className="site-link">
+              Women's Clothing
+            </Link>
+          </ul>
+        </div>
+        <div className="contact-section">
+          <img src={logo} alt="logo" />
+          <div className="contact-info">
+            <p>0431-12345678</p>
+            <p>Jl. Lorem ipsum dolor</p>
+          </div>
+          <div className="contact-logo">
+            <StyledLogo>
+              <IoLogoGithub />
+            </StyledLogo>
+            <StyledLogo>
+              <MdOutlineEmail />
+            </StyledLogo>
+          </div>
+        </div>
       </FooterContainer>
-      <div>copyright</div>
+      <CopyrightContainer>Copyright © {new Date().getFullYear()} Leuchtend</CopyrightContainer>
     </div>
   );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -5,20 +5,136 @@ const FooterContainer = styled.footer`
   grid-template-columns: repeat(3, 1fr);
   text-align: center;
   height: 200px;
-  margin-top: 100px;
+  margin-top: 150px;
   font-size: 1.2rem;
+  background-color: #a1a1a1;
+  color: white;
 
   .email-section {
-    background-color: pink;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 40px 60px;
+
+    .text-content {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+
+      & > p:nth-child(1) {
+        font-weight: bold;
+        font-size: 1.5rem;
+      }
+
+      & > p:nth-child(2) {
+        font-size: 1rem;
+      }
+    }
+
+    .input-content {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      input {
+        width: 70%;
+        height: 35px;
+        border: none;
+        outline: none;
+        font-size: 1rem;
+        padding: 5px;
+      }
+
+      button {
+        background-color: #2e2d2d;
+        border: none;
+        height: 35px;
+        cursor: pointer;
+        padding: 10px;
+        color: white;
+
+        &:hover {
+          background-color: #545454;
+        }
+      }
+    }
   }
 
   .site-section {
-    background-color: mintcream;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    align-items: center;
+
+    & > p {
+      font-weight: bold;
+      font-size: 1.5rem;
+    }
+
+    ul {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      font-size: 1rem;
+      width: fit-content;
+
+      .site-link {
+        text-decoration: none;
+
+        &:hover {
+          filter: brightness(30%);
+        }
+
+        &:visited {
+          color: white;
+        }
+      }
+    }
   }
 
   .contact-section {
-    background-color: orange;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+
+    img {
+      width: 120px;
+    }
+
+    .contact-info {
+      display: flex;
+      flex-direction: column;
+      gap: 5px;
+
+      font-size: 1rem;
+    }
+
+    .contact-logo {
+      display: flex;
+      gap: 10px;
+      font-size: 1.6rem;
+    }
   }
 `;
 
-export default FooterContainer;
+const CopyrightContainer = styled.div`
+  text-align: center;
+  background-color: #a1a1a1;
+  color: white;
+  padding-top: 60px;
+  padding-bottom: 20px;
+`;
+
+const StyledLogo = styled.div`
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(30%);
+  }
+`;
+
+export { FooterContainer, CopyrightContainer, StyledLogo };
