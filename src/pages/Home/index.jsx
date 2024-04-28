525ebb341998858b7adc00527219f5750c250e3a
leuchtend5 <zxcmj1@gmail.com>
2024-04-28 23:44:51 +0800

    feat(home): fetch and filter data for best seller

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,8 +11,15 @@ import { ProductCard, CategoryCard } from '../../components/Card';
 import useFetch from '../../hooks/useFetch';
 
 export default function Home() {
-  const url = 'https://fakestoreapi.com/products/categories';
-  const [data] = useFetch(url);
+  const urlCategories = 'https://fakestoreapi.com/products/categories';
+  const urlAllProducts = 'https://fakestoreapi.com/products';
+
+  const [dataCategory] = useFetch(urlCategories);
+  const [dataProduct] = useFetch(urlAllProducts);
+
+  function filterBySellCount(data) {
+    return data.filter((item) => item.rating.count > 400);
+  }
 
   return (
     <HomeContainer>
@@ -31,19 +38,29 @@ export default function Home() {
       <SecondSection>
         <h1>Shop By Category</h1>
         <div className="card-container">
-          {!data ? (
+          {!dataCategory ? (
             <h1>loading ...</h1>
           ) : (
-            data.map((category, index) => <CategoryCard key={index} category={category} />)
+            dataCategory.map((category, index) => <CategoryCard key={index} category={category} />)
           )}
         </div>
       </SecondSection>
       <SecondSection>
         <h1>Best Sellers</h1>
         <div className="card-container">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {!dataProduct ? (
+            <h1>loading ...</h1>
+          ) : (
+            filterBySellCount(dataProduct).map((item, index) => (
+              <ProductCard
+                key={index}
+                image={item.image}
+                name={item.title}
+                price={item.price}
+                rate={item.rating.rate}
+              />
+            ))
+          )}
         </div>
       </SecondSection>
     </HomeContainer>
