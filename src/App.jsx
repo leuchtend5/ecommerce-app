b1e1a3845fade339dbebef702c5b647b00473eb9
leuchtend5 <zxcmj1@gmail.com>
2024-05-01 22:42:06 +0800

    feat(app): add category provider in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Home from './pages/Home';
 import Products from './pages/Products';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { CategoryProvider } from './context/CategoryContext';
 
 export default function App() {
   return (
-    <>
+    <CategoryProvider>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="products" element={<Products />} />
+        <Route path="products" element={<Products />}>
+          <Route path="category/:category" element={<Products />} />
+        </Route>
       </Routes>
       <Footer />
-    </>
+    </CategoryProvider>
   );
 }
