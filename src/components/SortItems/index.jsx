8fd97e0dd484348cb4ce1bdff2528614ac382a0c
leuchtend5 <zxcmj1@gmail.com>
2024-05-05 19:55:12 +0800

    feat(component): add sortitem function inside component

--- src/components/SortItems/index.jsx
+++ src/components/SortItems/index.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { SortContainer, DropdownBox, DropdownAnimation } from './styles';
 import { RiArrowDropDownLine } from 'react-icons/ri';
+import PropTypes from 'prop-types';
 
-export default function SortItems() {
+export default function SortItems({ getSortValue }) {
   const [value, setValue] = useState('Price: High-Low');
   const [dropdownToggle, setDropdownToggle] = useState(false);
 
@@ -25,14 +26,53 @@ export default function SortItems() {
           </DropdownAnimation>
 
           <DropdownBox $dropdown={dropdownToggle}>
-            <div onClick={() => handleSortChange('Price: High-Low')}>Price: High-Low</div>
-            <div onClick={() => handleSortChange('Price: Low-High')}>Price: Low-High</div>
-            <div onClick={() => handleSortChange('Selling Count')}>Selling Count</div>
-            <div onClick={() => handleSortChange('Name: A-Z')}>Name: A-Z</div>
-            <div onClick={() => handleSortChange('Name: Z-A')}>Name: Z-A</div>
+            <div
+              onClick={() => {
+                handleSortChange('Price: High-Low');
+                getSortValue('Price: High-Low');
+              }}
+            >
+              Price: High-Low
+            </div>
+            <div
+              onClick={() => {
+                handleSortChange('Price: Low-High');
+                getSortValue('Price: Low-High');
+              }}
+            >
+              Price: Low-High
+            </div>
+            <div
+              onClick={() => {
+                handleSortChange('Selling Count');
+                getSortValue('Selling Count');
+              }}
+            >
+              Selling Count
+            </div>
+            <div
+              onClick={() => {
+                handleSortChange('Name: A-Z');
+                getSortValue('Name: A-Z');
+              }}
+            >
+              Name: A-Z
+            </div>
+            <div
+              onClick={() => {
+                handleSortChange('Name: Z-A');
+                getSortValue('Name: Z-A');
+              }}
+            >
+              Name: Z-A
+            </div>
           </DropdownBox>
         </span>
       </div>
     </SortContainer>
   );
 }
+
+SortItems.propTypes = {
+  getSortValue: PropTypes.func.isRequired,
+};
